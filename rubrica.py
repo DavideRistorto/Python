3a28613bd0599b3929e9fb3bffc3699819159ec6
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2021-11-04 11:45:22 +0100

    Add files via upload

---
+++ rubrica.py
@@ -0,0 +1,5 @@
+rubrica = {'p1': 1111, 'p2':2222, 'p3':3333}
+rubrica['p4'] = 4444    #aggiunge un nuova chiave e valore al dizionario
+
+print(rubrica['p1'])  #stampare un valore a scelta
+print(rubrica)
