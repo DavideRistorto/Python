f79707c14e631fa63aae965ebf5a33e04eb0774c
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2021-11-04 18:24:46 +0100

    Add files via upload

---
+++ stringa_ripetuta.py
@@ -0,0 +1,6 @@
+#stringa concatenata N volte
+string = str(input("metti una parola: "))  #input stringa e nmero
+num = int(input("metti un numero: "))
+
+string = string*num #string viene modicata con string*numVolte
+print(string)
