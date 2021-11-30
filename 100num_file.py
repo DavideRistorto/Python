3a4ab9d51c0cc1ae46506d19edd5b9fe9e0b7014
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2021-11-30 15:38:27 +0100

    Add files via upload

---
+++ 100num_file.py
@@ -0,0 +1,22 @@
+#stampare su file i primi 100 numeri primi
+def numeroPrimo( valore):
+    divisore = 2
+    primo = True  
+    
+    while (divisore<=valore/2) & (primo==True):
+        if valore%divisore == 0:
+            primo = False
+        else:
+           divisore =  divisore + 1 
+    return primo
+
+f = open("./numeriPrimi.txt","w")
+conta = 0
+num=0
+while conta<100:
+    if(numeroPrimo(num)==True):
+        f.write(str(num)+'\n') 
+        conta+=1
+    num+=1
+ 
+f.close()
